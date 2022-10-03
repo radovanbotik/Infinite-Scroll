7a97fad8d6278282caf24d96f5923261dc63845d
radovanbotik <radovanbotik@gmail.com>
2022-10-03 14:27:58 +0200

    comment

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let scrollingArea = innerblockWidth - sliderWidth;
 
 const clones = [];
 
+//
+// 1.Calculating scrollable area
 const getArea = () => {
   scrollingArea =
     innerblock.getBoundingClientRect().width -
@@ -15,6 +17,11 @@ const getArea = () => {
   return scrollingArea;
 };
 
+//
+// 1.Creating a deep clone for each item
+// 2.Pushing clone into new array
+// 3.Appending clone to my innerblock element
+// 4.Recalculating scrollable area after clones are injected
 const addClones = () => {
   items.forEach((item, index) => {
     const clone = item.cloneNode(true);
@@ -24,6 +31,10 @@ const addClones = () => {
   getArea();
 };
 
+//
+//1.listening to scroll
+//2.if inners block is moved equally or more than the scrollable area
+//  we run the function
 slider.addEventListener("scroll", e => {
   //   console.log(e.target.scrollLeft);
   if (e.target.scrollLeft >= scrollingArea) {
